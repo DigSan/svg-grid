5eabfd17bb5ac94aabf8f2438724b384794c5991
Александр <digsan@live.com>
2020-08-31 22:04:00 +0700

    1

--- src/features/chart/Chart.tsx
+++ src/features/chart/Chart.tsx
@@ -1,52 +1,65 @@
 import React, { useEffect } from 'react';
 import styles from './Chart.module.css';
+import { fromEvent, Subject, of, Observable } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
 interface Point { x: number, y: number };
+interface ChartState { zoom: number, position: Point }
 
-
-export class Chart extends React.Component<{}, { zoom: number }> {
+export class Chart extends React.Component<{}, ChartState> {
+    mouseUpDown = new Subject<number>();
+    mouseMove = new Subject<Point>();
+    testChart: Point[] = [
+        { x: 10, y: 20 },
+        { x: 20, y: 25 },
+        { x: 30, y: 15 },
+        { x: 35, y: 15 },
+        { x: 45, y: 5 }
+    ]
     constructor(props: any) {
         super(props);
 
-        this.state = { zoom: 0 };
-        useEffect(() => {
-            const clicky = fromEvent(this.chart.current, 'click').subscribe(clickety =>
-                console.log({ clickety })
-            );
-        }
-        );
+        this.state = { zoom: 1, position: { x: 0, y: 0 } };
 
+        this.mouseUpDown.pipe(
+            switchMap(x => x === 1 ? this.mouseMove : new Subject<Point>()))
+            .subscribe(x => {
+                this.setState({ zoom: this.state.zoom, position: { x: this.state.position.x + x.x, y: this.state.position.y + x.y } });
+            });
     }
-    chart = React.useRef(null);
+
     render() {
+        const baseWidth = 1500;
+        const baseHieght = 500;
 
-        const testChart: Point[] = [
-            { x: 10, y: 20 },
-            { x: 20, y: 25 },
-            { x: 30, y: 15 },
-            { x: 35, y: 15 },
-            { x: 45, y: 5 }
-        ]
 
-        const maxY = Math.max(...testChart.map(x => x.y));
-        const maxX = Math.max(...testChart.map(x => x.x));
-        const minX = Math.min(...testChart.map(x => x.x));
-        const minY = Math.min(...testChart.map(x => x.y));
+        const t2 = this.testChart.map((e) => `${e.x},${e.y}`).join(' ');
 
-        const t2 = testChart.map((e) => `${e.x},${e.y}`).join(' ');
+        const maxX = Math.max(...this.testChart.map(x => x.x));
+        const maxY = Math.max(...this.testChart.map(x => x.y));
+        const minX = Math.min(...this.testChart.map(x => x.x));
+        const minY = Math.min(...this.testChart.map(x => x.y));
+
+        const width = maxX / (Math.abs(this.state.zoom) + 1);
+        const hieght = maxY / (Math.abs(this.state.zoom) + 1);
+
+        const zoom = 1 / Math.pow(this.state.zoom, 2);
+
+        const viewBox = `${-this.state.position.x * zoom} ${-this.state.position.y * zoom} ${baseWidth * zoom} ${baseHieght * zoom}`;
 
         const divStyle = {
-            font: `italic ${(10 / (Math.abs(this.state.zoom) * 10 + 1) * 2)}px sans-serif`
+            font: `italic ${(14 * (Math.abs(zoom) ))}px sans-serif`
         };
 
-        const t1 = testChart.map(e => <text style={divStyle} x={e.x} y={e.y}>{this.state.zoom.toFixed(2)}</text>)
-
+        const t1 = this.testChart.map(e => <text style={divStyle} x={e.x} y={e.y}>{zoom.toFixed(2)}</text>)
 
         return (
-            <div className={styles.chart} onWheel={(e) => this.setState({ zoom: this.state.zoom + e.deltaY / 2000 })}>
-                <svg width="500" viewBox={`${minX} ${minY} ${maxX / Math.abs(this.state.zoom)} ${maxY / Math.abs(this.state.zoom)}`}>
-                    <line x1="1" x2=""></line>
-
+            <div className={styles.chart} onWheel={(e) => this.setState({ zoom: this.state.zoom - e.deltaY / 1000 })}
+                onMouseDown={() => this.mouseUpDown.next(1)}
+                onMouseUp={() => this.mouseUpDown.next(2)}
+                onMouseMove={e => this.mouseMove.next({ x: e.movementX, y: e.movementY })}
+            >
+                <svg width={baseWidth} height={baseHieght} viewBox={viewBox}>
                     <polyline points={t2} vectorEffect="non-scaling-stroke" fill="none" stroke="white" strokeWidth="1px" />
                     {t1}
                 </svg>
